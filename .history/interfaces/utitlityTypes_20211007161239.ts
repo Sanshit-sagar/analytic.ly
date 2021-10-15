fd85e5c4147f0d00469c24a8bfe72ce73efe0338
Sanshit Sagar <sasagar@ucsd.edu>
2021-10-14 17:02:33 -0700

    removing local history and next folders

--- .history/interfaces/utitlityTypes_20211007161239.ts
+++
@@ -1,7 +0,0 @@
-type Partial<T> = {
-    [P in keyof T]?: T[P];
-};
-
-type Readonly<T> = {
-    readonly [P in keyof T]: T[P];
-};
--- .history/lib/redis/index_20211007110140.js
+++
@@ -1,7 +0,0 @@
-import Redis from 'ioredis'
-Redis.Promise = require("bluebird");
-
-const redis = new Redis(REDIS_ENDPOINT)
-
-export default redis
-// enableAutoPipelining: true
